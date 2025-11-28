4967871c591fe2d315899b93ea551be8398f8696
20148528 <20148528@tafe.wa.edu.au>
2025-11-28 11:03:45 +0800

    Added unit test for display class

--- src/display.py
+++ src/display.py
@@ -1,13 +1,14 @@
 class Display:
-    def __init__(self, id, message = "", is_on = False):
+    def __init__(self, id, message = "Welcome to the car park.", is_on = True, car_park = None):
         self.id = id
         self.message = message
         self.is_on = is_on
+        self.car_park = car_park
 
     def __str__(self):
         return f"Display {self.id}: {self.message}"
 
     def update(self, data):
-        for key, value in data.items():
-            print(f"{key}: {value}")
+        if "message" in data:
+            self.message = data["message"]
 
