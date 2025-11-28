8008e5c34e46cb752eaf7af17c34e7521bb8c880
20148528 <20148528@tafe.wa.edu.au>
2025-11-28 15:44:47 +0800

    Add logging functionality

--- src/car_park.py
+++ src/car_park.py
@@ -1,13 +1,18 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
+
 
 class CarPark:
-    def __init__(self, location, capacity, plates = None, displays = None, sensors = None):
+    def __init__(self, location, capacity, plates = None, displays = None, sensors = None, log_file = Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.displays = displays or []
         self.sensors = sensors or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park at {self.location} with capacity {self.capacity} bays."
@@ -22,14 +27,20 @@ class CarPark:
             self.displays.append(component)
 
     def add_car(self, plate):
+        if self.available_bays == 0:
+            raise ValueError("Car park is full")
+        if plate in self.plates:
+            raise ValueError("Car already in car park")
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         if plate not in self.plates:
             raise ValueError()
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     def update_displays(self):
         for display in self.displays:
@@ -39,6 +50,10 @@ class CarPark:
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate}, {action} at {datetime.now()}\n")
 
     @property
     def available_bays(self):
